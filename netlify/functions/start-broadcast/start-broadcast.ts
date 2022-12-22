7fa5e777fa30e00372da7cf30a958e12e88e3ee3
Maarten Lakerveld <1911588+mlakerveld@users.noreply.github.com>
2022-12-22 16:20:04 +0000

    allow start of broadcast

---
+++ netlify/functions/start-broadcast/start-broadcast.ts
@@ -0,0 +1,25 @@
+import { Handler } from '@netlify/functions'
+import faunadb from 'faunadb'
+
+/* configure faunaDB Client with our secret */
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.FAUNA_KEY || "ERROR"
+})
+
+export const handler: Handler = async (event) => {
+    const data = JSON.parse(event.body ?? "{}")
+
+    let uuid = self.crypto.randomUUID();
+    let socketConn = self.crypto.randomUUID() + self.crypto.randomUUID();
+    await client.query(
+        q.Create('rooms', { data: { uuid: uuid, key: data.key, broadcastSock: socketConn } })
+      )
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        id: uuid,
+        socket: socketConn
+      }),
+    }
+}
